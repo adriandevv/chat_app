afe089b94de43ca7449ec35068a450b49e424068
Adrian Valencia Hernandez <adrian15996@gmail.com>
2025-02-06 14:52:08 -0600

    Agregar lógica para incluir rutas de canales en la configuración del servidor

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import authRoutes from './routes/AuthRouter.js';
 import contactRoutes from './routes/ContactRoutes.js';
 import { setupSocket } from './socket.js';
 import messagesRoutes from './routes/MessagesRouter.js';
+import channelRoutes from './routes/ChannelRoutes.js';
 dotenv.config();
 
 const app = express();
@@ -28,6 +29,8 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/contacts", contactRoutes);
 app.use("/api/messages", messagesRoutes);
+app.use("/api/channels", channelRoutes);
+
 
 const server = app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
